20fd99a71cf825e92226b1c4ef45e0d02ba3dabc
epradjonggo <epradjonggo@umass.edu>
2023-06-27 16:37:15 +0700

    login decrypt the encryption from frontend

--- service/login.py
+++ service/login.py
@@ -4,19 +4,28 @@ from .response import validation_response
 from .encrypt_decrypt import *
 from .loadPrivate import *
 import base64
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
 
 def login(collection, request):
     
     try:
         #load keys
+        private_key2 = load_privateKeys2()
         private_key = load_privateKeys()
         
         #from string to byte
         decodable1 = base64.b64decode(request["password"])
 
         #decrypt input 
-        password_input = decrypt(decodable1, private_key)
-        print(password_input)
+        rsa_key = RSA.importKey(private_key2)
+        cipher = PKCS1_v1_5.new(rsa_key)
+        decrypted = cipher.decrypt(decodable1, "ERROR")
+        password_input = decrypted.decode('utf-8')
+        
+        if not password_input:
+            raise Exception("Password input false")
+        
         #find the user
         query = {"name": request["name"]}
         cursor = collection.find(query)
@@ -26,11 +35,13 @@ def login(collection, request):
         user = list_cur[0]
 
         #from string to byte
-        decodable2 = base64.b64decode(request["password"])
+        decodable2 = base64.b64decode(user["password"])
 
         #decrypt db 
         password_db = decrypt(decodable2, private_key)
-        
+        if not password_db:
+            raise Exception("Password db false")
+
         if password_input != password_db:
             raise Exception("Password wrong")
 
