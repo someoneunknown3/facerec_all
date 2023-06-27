62edf187f5cb924a1b962c76ed9ce8b14a502a82
epradjonggo <epradjonggo@umass.edu>
2023-06-27 16:35:19 +0700

    add load key for frontend

--- service/loadPrivate.py
+++ service/loadPrivate.py
@@ -4,3 +4,7 @@ def load_privateKeys():
     with open('service/keys/privateKey.pem', 'rb') as p:
         privateKey = rsa.PrivateKey.load_pkcs1(p.read())
     return privateKey
+
+def load_privateKeys2():
+    privateKey = open('service/keys/private.pem', 'r').read()
+    return privateKey
--- service/loadPublic.py
+++ service/loadPublic.py
@@ -1,5 +1,6 @@
 import rsa
-import json
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
 
 def load_publicKeys():
     with open('service/keys/publicKey.pem', 'rb') as p:
@@ -7,18 +8,17 @@ def load_publicKeys():
     return publicKey
 
 def load_publicKeys2():
-    with open('service/keys/publicKey.pem', 'rb') as p:
-        publicKey = rsa.PublicKey.load_pkcs1(p.read())
+    publicKey = open('service/keys/public.pem', 'r').read()
     return publicKey
 
 def get_publicKey_str():
 
     # Load the public key
-    publicKey = load_publicKeys()
-    publicKey_str = publicKey.save_pkcs1().decode('utf-8')
+    publicKey = load_publicKeys2()
+    # publicKey_str = publicKey.save_pkcs1().decode('utf-8')
     # Create a JSON object containing the public key
     data = {
-        'publicKey': publicKey_str
+        'publicKey': publicKey
     }
     return data
     # # Serialize the JSON object to a string
