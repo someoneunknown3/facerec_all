c0d46e0b5384da4c6a81b538bcdd17b1b32d1178
epradjonggo <epradjonggo@umass.edu>
2023-07-13 16:02:54 +0700

    remove form.js content

--- static/shared.js
+++ static/shared.js
@@ -94,8 +94,7 @@ function create_error(messages){
   h5.innerHTML += "Alert"
 
   div.appendChild(h5)
-  div.appendChild(button)
-  console.log("messages: " + messages)
+  div.appendChild(button) 
   for(msg of messages){
     let text = document.createElement("div");
     text.innerHTML += "• " + msg
@@ -105,96 +104,4 @@ function create_error(messages){
   let body = document.body
   // let wrapper = document.getElementById("wrapper")
   body.appendChild(div)
-}
-
-function form_register_error(form){
-  let error = []
-  let newDict = {...form};
-
-  for(elem in form){
-    if(form[elem] == ""){
-      error.push(elem.charAt(0).toUpperCase() + elem.slice(1) + " is empty")
-      newDict[elem] = false
-    }
-    else{
-      newDict[elem] = true
-    }
-  }
-
-  if (form["password"] != "" && !passwordVerification(form["password"])){
-    newDict["password"] = false
-    newDict["retype"] = false
-    error.push("Password needs capital letters, number, and atleast 8 in length")
-  }
-  else if (form["password"] != form["retype"]){
-    newDict["retype"] = false
-    error.push("Retype needs to be the same as password")
-  }
-
-  return [error, newDict]
-}
-
-function form_login_error(form){
-  let error = []
-  let newDict = {...form};
-  for(elem in form){
-    if(form[elem] == ""){
-      error.push(elem.charAt(0).toUpperCase() + elem.slice(1) + " is empty")
-      newDict[elem] = false
-    }
-    else{
-      newDict[elem] = true
-    }
-  }
-  return [error, newDict]
-}
-
-function form_color(form){
-  for(elem in form){
-    if(form[elem]){
-      greenBox(elem)
-    }
-    else{
-      redBox(elem)
-    }
-  }
-}
-
-function redBox(str){
-  let tags = document.getElementsByName(str);
-  tags[0].style.border = "1px solid red";
-  tags[0].style.borderRight = "0";
-  tags[1].style.border = "1px solid red";
-  tags[1].style.borderLeft = "0";
-  tags[2].className = "";
-  tags[2].classList.add("fas", "fa-exclamation-circle")
-  tags[2].style.color = "red"
-}
-
-function greenBox(str){
-  let tags = document.getElementsByName(str);
-  console.log(tags[0])
-  tags[0].style.border = "1px solid green";
-  tags[0].style.borderRight = "0";
-  tags[1].style.border = "1px solid green";
-  tags[1].style.borderLeft = "0";
-  tags[2].className = "";
-  tags[2].classList.add("fas", "fa-check")
-  tags[2].style.color = "green"
-}
-
-function hasNumbers(inputString) {
-  return /\d/.test(inputString);
-}
-
-function hasUpper(inputString) {
-  return /[A-Z]/.test(inputString);
-}
-
-function lengthValid(inputString) {
-  return inputString.length >= 8;
-}
-
-function passwordVerification(password) {
-  return lengthValid(password) && hasNumbers(password) && hasUpper(password);
 }
