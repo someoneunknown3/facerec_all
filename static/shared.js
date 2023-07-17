a2b9f5de67554aac9a48ef727b770e7c9fd62781
epradjonggo <epradjonggo@umass.edu>
2023-07-17 09:46:43 +0700

    Update shared.js
    put logout in logged shared

--- static/shared.js
+++ static/shared.js
@@ -1,6 +1,5 @@
 async function verify(){
     let myValue = sessionStorage.getItem('token');
-    console.log(myValue)
     let user_id = null
     if(myValue != undefined){
       try {
@@ -103,95 +102,5 @@ function create_error(messages){
   }
 
   let body = document.body
-  // let wrapper = document.getElementById("wrapper")
   body.appendChild(div)
-}
-
-async function handleLogout(){
-  try {
-    sessionStorage.removeItem('token');
-    token = sessionStorage.getItem('token');
-
-    data = {
-      "success": (token == null)
-    }
-    const json = JSON.stringify(data);
-    const url = "logout"
-    await fetch(url, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: json,
-    })
-    .then(response =>{
-      if (response.ok) {
-        return response.json()
-      } else {
-        console.error('Error:', response.status);
-        console.error(response.json())
-      }
-    })
-    .then(jsonData =>{
-    })
-    .catch(function(err) {
-      console.info(err + " url: " + url)
-    });
-    
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
-
-function timeout(){
-  const TIMEOUT_IN_MS = 10 * 60 * 1000;
-  let timeoutHandle;
-
-  // Function to handle user activity
-  const handleUserActivity = () => {
-    // Clear the previous timeout
-    clearTimeout(timeoutHandle);
-
-    // Set a new timeout to prompt the user after the specified period of inactivity
-    timeoutHandle = setTimeout(() => {
-      // Prompt the user to confirm that they are still active
-      const isActive = confirm('Are you still there?');
-
-      if (isActive) {
-        // timeoutBox()
-      } else {
-          handleLogout()   
-      }
-    }, TIMEOUT_IN_MS);
-  };
-
-  // Listen for user activity events
-  document.addEventListener('mousemove', handleUserActivity);
-  document.addEventListener('keydown', handleUserActivity);
-}
-
-
-function interval(time=10){
-  // Set the interval period in milliseconds
-  const INTERVAL_IN_MS = time * 60 * 1000; // 1 minute
-
-  // Function to check the expiration time of the access token
-  const checkTokenExpiration = () => {
-    // Get the current time
-    let myValue = sessionStorage.getItem('token');
-    const currentTime = new Date().getTime();
-
-    // Get the expiration time of the access token
-    console.log(myValue)
-    const expirationTime = test; // retrieve the expiration time from storage
-
-    // Check if the access token is about to expire
-    if (currentTime >= expirationTime) {
-      // Refresh the access token
-      // ...
-    }
-  };
-
-  // Set an interval to periodically check the expiration time of the access token
-  setInterval(checkTokenExpiration, INTERVAL_IN_MS);
 }
